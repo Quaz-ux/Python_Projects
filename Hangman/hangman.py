0634e539771f093a0b34aca9fe66fcc388a94db3
Quaz-ux <szymon.anikiej@gmail.com>
2023-02-24 21:31:58 +0100

    Hangman game final

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,50 +1,42 @@
 import random
 import string
-
 from words import words
-
 
 def get_valid_word(words):
     word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
-
-    return word
-
+    return word.replace("-", " ").upper()
 
 def hangman():
     word = get_valid_word(words)
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
-
     lives = 6
 
     while len(word_letters) > 0 and lives > 0:
-        print('You have ', lives, ' lives left and you have used letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left and you have used letters:', ' '.join(used_letters))
         # current word
         word_list = [letter if letter in used_letters else '-' for letter in word]
-        print('Current word is: ', ' '.join(word_list))
+        print('Current word is:', ' '.join(word_list))
+
         user_letter = input('Guess a letter: ').upper()
-        if user_letter is alphabet - used_letters:
+        if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
-
             else:
-                lives = lives - 1
+                lives -= 1
                 print("Letter is not in the word.")
-
         elif user_letter in used_letters:
             print('You already used that character. Please type other character.')
-
         else:
-            print(f'Invalid character {user_letter}. Please type other character.')
+            print('Invalid character {}. Please type other character.'.format(user_letter))
 
     if lives == 0:
-        print('You died sorry. he word was: ', word)
+        print('You died sorry. The word was:', word)
     else:
-        print('You heve guessed the word: ', word)
+        print('You have guessed the word:', word)
 
-user_input = input('Type something: ')
 hangman()
